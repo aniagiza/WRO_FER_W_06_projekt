3f3a3da855d30eb1cdcab612e7ede1e098ee2010
Anna Giża <ania.giza@tlen.pl>
2018-10-14 10:27:00 +0200

    Gwiazdeczka :-)

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
+import styles from "./App.scss";
 import { Parallax } from "react-spring"
+import Sky from "../sky/Sky";
+import Star from "../star/Star";
 
 const App = () => (
-    <Parallax pages={2}>
+    <div className={ styles.app }>
+        <Parallax pages={2}>
+            <Parallax.Layer offset={0} speed={0}><Sky /></Parallax.Layer>
 
-    </Parallax>
+            <Parallax.Layer offset={ 0.5 } speed={ -1 }><Star /></Parallax.Layer>
+        </Parallax>
+    </div>
 );
 
 export default App;
